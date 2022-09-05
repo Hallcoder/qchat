1f9bc7dfd14ccc740cce1d73e27d1ac4186dd681
Hallcoder <hallcoder25@gmail.com>
2022-09-05 10:57:31 +0200

    lesson: you can use id property of a widget as an alternative to avoid typo errors in named routes

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:qchat/screens/welcome_screen.dart';
+import 'package:qchat/screens/login_screen.dart';
+import 'package:qchat/screens/registration_screen.dart';
+import 'package:qchat/screens/chat_screen.dart';
+
+void main() => runApp(FlashChat());
+
+class FlashChat extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+        textTheme: const TextTheme(
+          bodyText1: TextStyle(color: Colors.black54),
+        ),
+      ),
+      initialRoute: WelcomeScreen().id,
+      routes:{
+        WelcomeScreen().id:(context) => WelcomeScreen(),
+        '/chat':(context) => ChatScreen(),
+        '/login':(context) => LoginScreen(),
+        '/register':(context) => RegistrationScreen(),
+      },
+    );
+  }
+}
