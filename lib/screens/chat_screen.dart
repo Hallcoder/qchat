3b13fe1304507734650978a0eb10b33c5e355ec3
Hallcoder <hallcoder25@gmail.com>
2022-09-08 11:41:29 +0200

    good looking ui for the sake of doing so

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -2,6 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:qchat/constants.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+final _firestore = FirebaseFirestore.instance;
 class ChatScreen extends StatefulWidget {
   static String id = '/chat';
   @override
@@ -9,8 +10,8 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatScreenState extends State<ChatScreen> {
+  final messageTextController = TextEditingController();
   final _auth = FirebaseAuth.instance;
-  final _firestore = FirebaseFirestore.instance;
   late User loggedInUser;
   late String messageText = '';
 
@@ -19,24 +20,27 @@ class _ChatScreenState extends State<ChatScreen> {
     super.initState();
     getCurrentUser();
   }
-void messageStream() async{
-   await for(var snapshot in _firestore.collection('Messages').snapshots()){
-     for(var message in snapshot.docs){
-       print(message.data());
-     };
-   };
-}
-  void getCurrentUser() async{
-    final user =  _auth.currentUser;
-    try{
-      if(user != null){
+
+  void messageStream() async {
+    await for (var snapshot in _firestore.collection('Messages').snapshots()) {
+      for (var message in snapshot.docs) {
+        print(message.data());
+      }
+    }
+  }
+
+  void getCurrentUser() async {
+    final user = _auth.currentUser;
+    try {
+      if (user != null) {
         loggedInUser = user;
         print(loggedInUser.email);
       }
-    }catch(e){
+    } catch (e) {
       print(e);
     }
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -44,14 +48,14 @@ void messageStream() async{
         leading: null,
         actions: <Widget>[
           IconButton(
-              icon: Icon(Icons.close),
+              icon: const Icon(Icons.close),
               onPressed: () {
                 messageStream();
-               // _auth.signOut();
+                // _auth.signOut();
                 // Navigator.pop(context);
               }),
         ],
-        title: Text('⚡️Chat'),
+        title: const Text('⚡️Chat'),
         backgroundColor: Colors.lightBlueAccent,
       ),
       body: SafeArea(
@@ -59,13 +63,13 @@ void messageStream() async{
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
+           MessageStream(),
             Container(
-              decoration: kMessageContainerDecoration,
               child: Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: <Widget>[
+                children: [
                   Expanded(
                     child: TextField(
+                      controller: messageTextController,
                       onChanged: (value) {
                         messageText = value;
                         //Do something with the user input.
@@ -74,26 +78,97 @@ void messageStream() async{
                     ),
                   ),
                   FlatButton(
-                    onPressed: () async{
-                      print('method caleed');
-                    var data  =   await _firestore.collection('Messages').add({
-                        "sender":loggedInUser.email,
-                        "text":messageText
-                      });
-                    print(data);
+                    onPressed: () async {
+                      messageTextController.clear();
+                      print('method called');
+                      var data = await _firestore.collection('Messages').add(
+                          {"sender": loggedInUser.email, "text": messageText});
+                      print(data);
                       //Implement send functionality.
                     },
-                    child: Text(
+                    child: const Text(
                       'Send',
                       style: kSendButtonTextStyle,
                     ),
                   ),
                 ],
               ),
-            ),
+            )
           ],
         ),
       ),
     );
   }
 }
+
+class MessageBubble extends StatelessWidget {
+  const MessageBubble({Key? key, required this.sender, required this.text})
+      : super(key: key);
+  final String sender;
+  final String text;
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          Text(sender,style: const TextStyle(
+              color: Colors.blueGrey,
+              fontSize: 10.0
+          ),),
+          Material(
+            borderRadius: BorderRadius.circular(10.0),
+            elevation: 5.0,
+            color: Colors.lightBlueAccent,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 10.0,horizontal: 20.0),
+              child: Text(text,style: const TextStyle(
+                fontSize: 15.0,
+                color:Colors.white
+              ),),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class MessageStream extends StatelessWidget {
+  const MessageStream({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return  StreamBuilder<QuerySnapshot>(
+      stream: _firestore.collection('Messages').snapshots(),
+      builder: (buildContext, snapshot) {
+        List<MessageBubble> messageBubbles = [];
+        if (!snapshot.hasData) {
+          return const Center(
+            child: CircularProgressIndicator(
+              backgroundColor: Colors.lightBlueAccent,
+            ),
+          );
+        }
+        final messages = snapshot.data?.docs;
+        for (var message in messages!) {
+          final messObject = message.data() as Map;
+          final messageText = messObject['text'];
+          final messageSender = messObject['sender'];
+          final messageBubble =
+          MessageBubble(sender: messageSender, text: messageText);
+          messageBubbles.add(messageBubble);
+        }
+        return Expanded(
+          child: ListView(
+            padding:
+            const EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
+            children: messageBubbles,
+          ),
+        );
+      },
+    );
+  }
+}
+
